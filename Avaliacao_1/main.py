301178e8c3a913ac141a1f07521809eac32ae04e
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-05-04 23:11:47 -0300

    fixed main

--- Avaliacao_1/main.py
+++ Avaliacao_1/main.py
@@ -12,3 +12,7 @@ d1 = Desktop( "PC Intel Core i7", "cinza", 6.425 , "500W")
 
 print(f"O preço do Notebook é: R${n1.cadastrar(n1)},00")
 print(f"O modelo do Desktop é: {d1.modelo}")
+
+n1.getInformacoes()
+
+d1.getInformacoes()
