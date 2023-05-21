e68399dbcfca493702a7cca104f3e577b0e541e5
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-05-21 17:06:19 -0300

    Add Lista, main and No lista encadeada exercicio

---
+++ exercicio_aula_08/Lista.py
@@ -0,0 +1,83 @@
+from No import No
+
+class Lista:
+    def __init__(self):
+        self.inicio = None
+        self.tamanho = 0
+    
+    def addInicio(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        else:
+            nodo.proximo = self.inicio
+            self.inicio = nodo
+        self.tamanho +=1
+        self.imprimir()
+
+    def addFim(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        else:
+            aux = self.inicio
+            while aux.proximo:
+                aux = aux.proximo
+            aux.proximo = nodo
+        self.tamanho += 1
+        self.imprimir()
+    
+    def imprimir(self):
+        if self.inicio == None:
+            print("Lista vazia")
+        else:
+            print("------------")
+            aux = self.inicio
+            while( aux ):
+                print(aux.dado)
+                aux = aux.proximo
+            print("Total de elementos: ", str(self.tamanho))
+
+    def removerInicio(self):
+        if self.inicio == None:
+            print("Lista vazia")
+        elif self.inicio.proximo == None:
+            self.inicio = None
+            self.tamanho -= 1
+        else:
+            self.inicio = self.inicio.proximo
+        self.tamanho -= 1
+    
+    def removerFim(self):
+        if self.inicio == None:
+            print("Lista vazia")
+        elif self.inicio.proximo == None:
+            self.inicio = None
+            self.tamanho = 0
+        else:
+            ant = self.inicio
+            aux = self.inicio.proximo
+            while aux.proximo:
+                ant = aux
+                aux = aux.proximo
+            ant.proximo = None
+            self.tamanho -= 1
+
+    def addOrdenado(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        elif valor < self.inicio.dado:
+            nodo.proximo = self.inicio
+            self.inicio = nodo
+        else:
+            atual = self.inicio
+            while atual.proximo and valor > atual.proximo.dado:
+                atual = atual.proximo
+            nodo.proximo = atual.proximo
+            atual.proximo = nodo
+        self.tamanho += 1
+        self.imprimir()
+        
+
+        
---
+++ exercicio_aula_08/main.py
@@ -0,0 +1,9 @@
+from Lista import Lista
+
+lista = Lista()
+
+
+lista.addOrdenado(5)
+lista.addOrdenado(2)
+lista.addOrdenado(7)
+lista.addOrdenado(1)
