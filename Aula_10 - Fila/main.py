7d46c06ecf73af2a0a18183499a7de3361616b34
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-06-05 20:22:41 -0300

    Aula_10 - Fila

---
+++ Aula_10 - Fila/main.py
@@ -0,0 +1,17 @@
+from Fila import Fila
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.add("João")
+fila.add("Maria")
+fila.add("Júlia")
+fila.remover()
+fila.add("Carlos")
+fila.remover()
+fila.remover()
+fila.remover()
+
+
+
