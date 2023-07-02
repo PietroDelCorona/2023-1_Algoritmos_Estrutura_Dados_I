cd0c1a21d6ab60a5013eb6e4ae8609355423ed3b
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-07-02 17:17:31 -0300

    Exercicio Lista Duplamente Encadeada

---
+++ Exercicios_praticos/Lista_Duplamente_Encadeada/main.py
@@ -0,0 +1,20 @@
+from Lista import Lista
+
+lista = Lista()
+
+
+lista.addInicio(10)
+lista.addInicio(5)
+lista.addFim(15)
+
+lista.imprimir()
+
+print("-----------------")
+
+lista.imprimirReverso()
+
+print("-----------------")
+
+lista.removerInicio()
+
+lista.removerFim()
